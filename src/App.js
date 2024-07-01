526431ade44788e3c7b9076ee2c657129a204b93
trena <20079502+tholst952@users.noreply.github.com>
2024-07-01 16:24:52 -0700

    added array, beginning component plans

--- src/App.js
+++ src/App.js
@@ -1,3 +1,38 @@
 import "./index.css";
+import { characters } from "./array.js";
 
-export default function App() {}
+export default function App() {
+  return (
+    <div className="main">
+      <Heading />
+      <Sidebar characters={characters} />
+    </div>
+  );
+}
+
+function Button() {
+  return <button></button>;
+}
+
+function Heading() {
+  return (
+    <div>
+      <h1 className="main-heading">Stardew Gift Guide 🎁</h1>
+    </div>
+  );
+}
+
+function Sidebar({ characters }) {
+  return characters.map((char) => (
+    <div className="card" key={char.name}>
+      <img src={require(`${char.image}`)} alt={char.name} />
+      {/* <p>{char.birthday}</p> */}
+
+      <div className="char-info">
+        <h3 className="char-name">{char.name}</h3>
+        <p className="char-title">{char.title}</p>
+        <button>Select</button>
+      </div>
+    </div>
+  ));
+}
